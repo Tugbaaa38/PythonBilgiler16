5b529e46491f5722e69fdb4de6267d03f24ac0f5
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-03-16 12:18:30 +0300

    Update python17.py

--- python17.py
+++ python17.py
@@ -14,7 +14,7 @@ personel1=Personel("Ali","Asaf","12354879","Kayseri","05002145484")
 personel2=Personel("Eylul","Can","14658793","Ankara","05103214565")
 
 print(personel1.ad)
-print(personel1.sehir)
+print(personel1.sehir)            #Ali-Kayseri
 print("\n")
-print(personel2.ad)
+print(personel2.ad)               #Eylul-Ankara
 print(personel2.sehir)
