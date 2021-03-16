68600ebe3b2706cf7a16079cd8d6b551379f6225
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-03-16 12:17:50 +0300

    Add files via upload

---
+++ python17.py
@@ -0,0 +1,20 @@
+
+#init ve self
+#kolayca ornekleyelim..
+
+class Personel():
+    def __init__(self,ad,soyad,perNo,sehir,telNo):
+        self.ad=ad
+        self.soyad=soyad
+        self.perNo=perNo
+        self.sehir=sehir
+        self.telNo=telNo
+
+personel1=Personel("Ali","Asaf","12354879","Kayseri","05002145484")
+personel2=Personel("Eylul","Can","14658793","Ankara","05103214565")
+
+print(personel1.ad)
+print(personel1.sehir)
+print("\n")
+print(personel2.ad)
+print(personel2.sehir)
